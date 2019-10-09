cbcf0eeb16eb0aa80fb953a0ab56a2148c075918
badubizzle <badu.boahen@gmail.com>
2019-10-09 00:46:44 +0000

    Update tests

--- test/word_number_test.exs
+++ test/word_number_test.exs
@@ -61,4 +61,47 @@ defmodule WordNumberTest do
     assert Numbers.words_to_number("million") == 1_000_000
     assert Numbers.words_to_number("billion") == 1_000_000_000
   end
+
+  test "numbers to words" do
+    assert "one" == Numbers.number_to_words(1)
+    assert "ten" == Numbers.number_to_words(10)
+    assert "one hundred" == Numbers.number_to_words(100)
+    assert "one thousand" == Numbers.number_to_words(1_000)
+    assert "one million" == Numbers.number_to_words(1_000_000)
+
+    assert "one hundred twenty three million one hundred twenty three thousand one hundred twenty three" ==
+             Numbers.number_to_words(123_123_123)
+
+    assert "one hundred ninety nine billion two hundred fifty four million seven hundred forty thousand nine hundred ninety two" ==
+             Numbers.number_to_words(199_254_740_992)
+
+    assert Numbers.number_to_words(135) == "one hundred thirty five"
+
+    assert Numbers.number_to_words(2_023_049) ==
+             "two million twenty three thousand forty nine"
+
+    assert Numbers.number_to_words(2_003_984) ==
+             "two million three thousand nine hundred eighty four"
+
+    assert Numbers.number_to_words(2_003_984) ==
+             "two million three thousand nine hundred eighty four"
+
+    assert Numbers.number_to_words(19) == "nineteen"
+    assert Numbers.number_to_words(2019) == "two thousand nineteen"
+    assert Numbers.number_to_words(2_003_019) == "two million three thousand nineteen"
+    assert Numbers.number_to_words(3_000_000_000) == "three billion"
+    assert Numbers.number_to_words(3_000_000) == "three million"
+
+    assert Numbers.number_to_words(123_456_789) ==
+             "one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine"
+
+    assert Numbers.number_to_words(11) == "eleven"
+    assert Numbers.number_to_words(19_000_000_019) == "nineteen billion nineteen"
+
+    assert Numbers.number_to_words(2_023_049) ==
+             "two million twenty three thousand forty nine"
+
+    assert Numbers.number_to_words(1_002_023_049) ==
+             "one billion two million twenty three thousand forty nine"
+  end
 end
