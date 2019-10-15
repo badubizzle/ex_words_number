3def15225e8054d1e3d33cb1526d547d3ab728a3
badubizzle <badu.boahen@gmail.com>
2019-10-15 01:34:18 +0000

    Adds test for invalid figure words

--- test/word_number_test.exs
+++ test/word_number_test.exs
@@ -5,6 +5,28 @@ defmodule WordNumberTest do
 
   use ExUnitProperties
 
+  test "invalid words" do
+    words = [
+      "one two hundred",
+      "forty two hundred",
+      "thousand one",
+      "forty eleven",
+      "four eleven",
+      "one thirteen"
+    ]
+
+    for w <- words do
+      try do
+        Numbers.words_to_number(w)
+        assert false, "Invalid error"
+      rescue
+        e ->
+          IO.inspect(e)
+          assert true
+      end
+    end
+  end
+
   property "number to words and words to number" do
     check all(number <- positive_integer() |> resize(1_000)) do
       words = Numbers.number_to_words(number)
